978f11758b4536444e2eafcf2d32857133e8f4c1
key36ra <cfah3618@gmail.com>
2018-03-12 16:57:51 +0900

    2018-03-12-16:57:51

---
+++ os_setup.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+function func_os_set(){
+	# Variable
+	dir=rasp_os
+	sd=/dev/mmcblk0 # not partition but device
+	os=CentOS-Userland-7-armv7hl-Minimal-1708-RaspberryPi3.img
+	# Make working directory
+	mkdir $dir
+	cd $dir
+	# Write image to sd
+	wget http://buildlogs.centos.org/centos/7/isos/armhfp/$os.xz # download image
+	xz -d $os.xz # resolve compressed image
+	sudo dd bs=8192 if=$os of=$sd # write image to sd
+	# Delete working directory
+	cd ../
+	rm -r $dir
+}
+
+function func_setting(){
+	sd=/dev/mmcblk0
+	start=1000M
+	end=28G
+	#uuid=
+	#pass=
+	
+	# Keyboard setting
+	localectl set-keymap jp106
+	localectl set-keymap jp-OADG109A
+	
+	# Expand the root partition
+	parted $sd # into parted interface at sd
+	rm 3
+	mkpart primary $start $end
+	quit
+	reboot
+	resize2fs ${sd}p3
+	
+	# Wifi setting
+	#nmcli d # confine the network connection
+	#nmcli d wifi # confine the wifi accesspoint
+	#nmcli d wifi connect $uuid password $pass
+	#nmcli d show wlan0 # confine own ip by wlan0
+	
+	# Package db update
+	yum update
+	
+	# User and Pass setting
+	passwd root
+	m_user=centm_ser0
+	useradd $m_user
+	passwd $m_user
+	usermod ^G wheel $m_user # add user to Manager
+	# only Manager ==> root
+}
+
+function func_manage(){
+	# Shutdown os
+	poweroff
+	# Reboot
+	reboot
+}
