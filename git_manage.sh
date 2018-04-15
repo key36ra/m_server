2bddc3e76cd106d4f65ea622f979eaecdf334337
key36ra <kimdoubt96@gmail.com>
2018-04-15 13:21:13 +0900

    2018-04-15-13:21:12

--- git_manage.sh
+++ git_manage.sh
@@ -2,10 +2,10 @@
 
 ct="$(date +'%Y-%m-%d-%H:%M:%S')"
 
+# Confirm file of update commit
+echo $ct >> com_update
+
 # Management
 git add .
 git commit -m $ct
 git push
-
-# Confirm file of update commit
-echo $ct >> com_update
