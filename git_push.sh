2e04bc0fc616052d9e703a729f2e7898a1223b9d
key36ra <kimdoubt96@gmail.com>
2018-05-01 02:28:06 +0900

    2018-05-01-02:28:05

--- git_push.sh
+++ git_push.sh
@@ -3,7 +3,8 @@
 ct="$(date +'%Y-%m-%d-%H:%M:%S')"
 
 # Confirm file of update commit
-echo -e "\n$ct" >> update_confirm
+#echo -e "\n$ct" >> update_confirm
+echo $ct >> update_confirm
 
 # Management
 git add .
