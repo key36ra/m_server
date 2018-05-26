888974b6c8346e67f6e103d765b04abc79eced77
key36ra <kimdoubt96@gmail.com>
2018-05-27 01:11:11 +0900

    2018/5/27-1:11:10

--- git_push.py
+++ git_push.py
@@ -6,8 +6,9 @@ from datetime import datetime
 now = datetime.now()
 commit = "{}/{}/{}-{}:{}:{}".format(now.year, now.month, now.day, now.hour, now.minute, now.second)
 
-file = open('update_confirm', 'w')
+file = open('update_confirm', 'a')
 file.write(commit)
+file.close()
 
 os.system('git add .')
 os.system('git commit -m {}'.format(commit))
